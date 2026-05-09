5942d58746c476d41a696e22328a596f614d30d9
venkateshwarreddy25 <mvreddy052005@gmail.com>
2026-05-10 00:54:08 +0530

    Fix uuid ESM error by using native crypto

--- server/models/Board.js
+++ server/models/Board.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const { v4: uuidv4 } = require('uuid')
+const { randomUUID } = require('crypto')
 
 const BoardSchema = new mongoose.Schema({
   name: {
@@ -11,7 +11,7 @@ const BoardSchema = new mongoose.Schema({
   shareId: {
     type: String,
     unique: true,
-    default: () => uuidv4(),
+    default: () => randomUUID(),
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
